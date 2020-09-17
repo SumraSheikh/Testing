534d072871807857e2a1c38d0415266dbc13bdac
unknown <sumra0102@gmail.com>
2020-09-17 14:36:21 +0500

    test

---
+++ sqOfNum.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 25 19:43:21 201
+"""
+
+x = int(input("Enter 1st number:"))
+y = int(input("Enter 2nd number:"))
+z = x*x
+if(y==z):
+    print(y," is the square of",x)
+else:
+     print("input again")
+    
+
+    
